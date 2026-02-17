5caf2c940d26424c6a502faa20cabcd98000723d
Nabara99 <rtaufiq212@gmail.com>
2026-02-17 16:10:07 +0800

    captcha just plus

--- app/Http/Controllers/CaptchaController.php
+++ app/Http/Controllers/CaptchaController.php
@@ -8,43 +8,14 @@ class CaptchaController extends Controller
 {
     public function generate()
     {
-        $operators = ['+', '-', '*', '/'];
-        $operator = $operators[array_rand($operators)];
-
-        // Generate numbers based on operator to keep results clean
-        switch ($operator) {
-            case '/':
-                $num2 = rand(1, 9);
-                $result = rand(1, 9);
-                $num1 = $num2 * $result; // Ensures clean division
-                break;
-            case '*':
-                $num1 = rand(1, 9);
-                $num2 = rand(1, 9);
-                $result = $num1 * $num2;
-                break;
-            case '-':
-                $num1 = rand(1, 9);
-                $num2 = rand(1, $num1); // Ensures positive result
-                $result = $num1 - $num2;
-                break;
-            default: // +
-                $num1 = rand(1, 9);
-                $num2 = rand(1, 9);
-                $result = $num1 + $num2;
-                break;
-        }
+        $num1 = rand(1, 9);
+        $num2 = rand(1, 9);
+        $result = $num1 + $num2;
 
         Session::put('captcha_result', $result);
 
-        $symbol = match ($operator) {
-            '*' => '×',
-            '/' => '÷',
-            default => $operator,
-        };
-
         return response()->json([
-            'question' => "{$num1} {$symbol} {$num2} = ?",
+            'question' => "{$num1} + {$num2} = ?",
         ]);
     }
 }
